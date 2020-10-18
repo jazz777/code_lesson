a119690901ab96f325159149b359ca7623bd8cfb
EC2 Default User <ec2-user@ip-172-31-21-31.ap-northeast-1.compute.internal>
2020-10-18 07:23:28 +0000

    cloud9

---
+++ java/java_1/Nozomi.java
@@ -0,0 +1,10 @@
+class Nozomi {
+public static void main (String[] args){
+System.out.println("Nozomi kawaii");
+System.out.println("Nozomi kawaii");
+System.out.println("Nozomi kawaii");
+System.out.println("Nozomi kawaii");
+System.out.println("Nozomi kawaii");
+System.out.println("Nozomi kawaii");
+}
+}
