bb2d3e0bb96155322b99102f13bdfed8b398a6a7
Taiichi Fujiwara <taiichi930@gmail.com>
2020-11-14 11:35:45 +0000

    file path change

--- python/test.py
+++ python/test.py
@@ -1,4 +1,5 @@
 import json
 dic={'aize': 'M', 'name': 'nozomi'}
-with open('/home/taiichi930/file1','r+') as fp:
+#with open('/home/taiichi930/file1','r+') as fp:
+with open('/home/taiichi930/code_lesson/python/file','w+') as fp:
     fp.write(json.dumps(dic,indent=4))
